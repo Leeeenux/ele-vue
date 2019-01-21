c06bd3f08e0c364a406ce999fa69684e0dc29681
Andsoon96 <79742066@qq.com>
2019-01-21 15:17:38 +0800

    提交

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="sidebar">
+    <el-menu default-active="onRoutes" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse"
+      background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+      <!-- unique-opened router -->
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item-group>
+          <span slot="title">分组一</span>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <span slot="title">选项4</span>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+
+
+<style>
+  .sidebar {
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 80px;
+    bottom: 0;
+    overflow-y: scroll;
+  }
+
+  .sidebar::-webkit-scrollbar {
+    width: 0;
+  }
+
+  .sidebar-el-menu:not(.el-menu--collapse) {
+    width: 250px;
+  }
+
+  .sidebar>ul {
+    height: 100%;
+  }
+
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+</style>
+
+<script>
+  import bus from '@/components/bus';
+
+  export default {
+    name: "Sidebar",
+    data() {
+      return {
+        isCollapse: false
+      };
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    computed: {
+      onRoutes() {
+        return this.$route.path.replace('/', '');
+      }
+    },
+    created() {
+      // 通过 Event Bus 进行组件间通信，来折叠侧边栏
+      bus.$on('isCollapse', msg => {
+        this.isCollapse = msg;
+      })
+    }
+  };
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <Sidebar />
+    <div class="content-box" :class="{'content-collapse':isCollapse}">
+      <div class="content">
+        <transition name="move" mode="out-in">
+          <keep-alive :include="tagsList">
+            <router-view></router-view>
+          </keep-alive>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import Header from '@/components/common/Header.vue'
+  import Sidebar from '@/components/common/Sidebar.vue'
+  import bus from '@/components/bus'
+
+  export default {
+    name: 'home',
+    data() {
+      return {
+        isCollapse: false,
+        tagsList: []
+      }
+    },
+    components: {
+      Header,
+      Sidebar
+    },
+    created() {
+      bus.$on('isCollapse', msg => {
+        this.isCollapse = msg;
+      })
+    }
+  }
+</script>
