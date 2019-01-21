8df1857a9e53640fce8463c79a7487a9b94815dc
Andsoon96 <79742066@qq.com>
2019-01-21 15:19:29 +0800

    pages

---
+++ src/components/pages/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+    <div class="wrapper">
+      <div class="collapse-btn" @click="collapseChage">
+        <i class="el-icon-menu"></i>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+
+
+<script>
+  import bus from '@/components/bus';
+
+  export default {
+    name: 'Header',
+    data() {
+      return {
+        isCollapse: false
+      }
+
+    },
+    methods: {
+      // 侧边栏折叠
+      collapseChage() {
+        this.isCollapse = !this.isCollapse;
+        console.log(this.isCollapse)
+        bus.$emit('isCollapse', this.isCollapse);//bus.$emit用来实现组件之间的通信，待研究
+      }
+    },
+    mounted() {
+      if (document.body.clientWidth < 1500) {
+        this.collapseChage();
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .wrapper {
+    width: 100%;
+    height: 80px;
+    background-color: red;
+  }
+</style>
